8d10bbde8d9f63761da4c204aebd0caf7eae0e55
utuniyal12 <utuniyal@gmail.com>
2021-05-24 01:03:44 +0530

    almost there

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 var enterButton = document.getElementsByClassName("enter-btn")[0];
 var intro = document.getElementsByClassName("intro")[0];
 var main = document.getElementsByClassName("main")[0];
-
-
-
-
-enterButton.addEventListener("click", function(){
-    intro.className+=" hidden"
+var bannerOptions = document.getElementsByClassName("banner-options")[0];
+var techstackbtn = document.getElementById("tech-stack-btn");
+var bannerHeading = document.getElementsByClassName("banner-message")[0].getElementsByTagName("h1")[0];
+var teckStackMsg1 = document.getElementsByClassName("tc-st1")[0];
+var teckStackMsg2 = document.getElementsByClassName("tc-st2")[0];
+var skillscube = document.getElementsByClassName("skills")[0];
+var banner =document.getElementsByClassName("banner")[0];
+enterButton.addEventListener("click", function () {
+    intro.className += " hidden"
     main.className += "reveal"
-    main.className = main.className.replace("hidden"," ")
+    main.className = main.className.replace("hidden", " ")
 })
 
-
-
-var bannerMessage = document.getElementsByClassName("banner-message")[0].getElementsByTagName("h1")[0];
-
-bannerMessage.addEventListener()
+techstackbtn.addEventListener('click', function () {
+    bannerHeading.className += " hidden"
+    bannerOptions.className += " hidden"
+    teckStackMsg1.className= teckStackMsg1.className.replace("hidden","")
+    setTimeout(function(){
+        teckStackMsg1.className += " hidden"
+        teckStackMsg2.className = teckStackMsg2.className.replace("hidden","")
+        setTimeout(function(){
+            banner.className += " hidden"
+            skillscube.className += " reveal"
+            skillscube.setAttribute("style","display:flex;")
+        },4000)
+    },4000)
+})
